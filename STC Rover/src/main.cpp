a27064495fe1e67e19bf184dc425fec31f52f556
Sethricks340 <slricks340@byui.edu>
2026-01-31 09:51:06 -0700

    PWM works

--- STC Rover/src/main.cpp
+++ STC Rover/src/main.cpp
@@ -3,8 +3,16 @@
 
 const int IN1 = 19;
 const int IN2 = 18;
-const int IN3 = 5;
-const int IN4 = 4;
+const int IN3 = 25;
+const int IN4 = 26;
+
+const int enableA = 21;
+const int enableB = 27;
+
+const int freq = 5000;
+const int channelA = 0;
+const int channelB = 1;
+const int resolution = 8;
 
 const char* ssid = "Threat Level Midnight";
 const char* password = "cowabunga2!!";
@@ -19,14 +27,18 @@ WiFiServer server(80);
 
 int ledPin = 2;  // built-in LED
 
-void motor_on(int number){
+void motor_on(int number, int pwm){
   if (number){
     digitalWrite(IN3, LOW);
     digitalWrite(IN4, HIGH);
+    ledcWrite(channelB, pwm); 
+    // ledcWrite(channelB, 255); 
   }
   else {
     digitalWrite(IN1, LOW);
     digitalWrite(IN2, HIGH);
+    ledcWrite(channelA, pwm); 
+    // ledcWrite(channelA, 255); 
   }
 }
 
@@ -34,15 +46,23 @@ void motor_off(int number){
   if (number){
     digitalWrite(IN3, LOW);
     digitalWrite(IN4, LOW);
+    ledcWrite(channelB, 0);
   }
   else {
     digitalWrite(IN1, LOW);
     digitalWrite(IN2, LOW);
+    ledcWrite(channelA, 0);
   }
 }
 
 void setup() {
   Serial.begin(115200);
+
+  ledcSetup(channelA, freq, resolution);
+  ledcAttachPin(enableA, channelA);
+
+  ledcSetup(channelB, freq, resolution);
+  ledcAttachPin(enableB, channelB);
 
   pinMode(ledPin, OUTPUT);
   pinMode(IN1, OUTPUT);
@@ -73,14 +93,32 @@ void loop() {
   if (!client) return;
 
   String req = client.readStringUntil('\r');
+  req.trim();
   client.flush();
 
-  // Get Request
-  if (req.indexOf("/motor/0/on") != -1)  motor_on(0);
-  if (req.indexOf("/motor/1/on") != -1)  motor_on(1);
-  
-  if (req.indexOf("/motor/0/of") != -1)  motor_off(0);
-  if (req.indexOf("/motor/1/of") != -1)  motor_off(1);
+// Get Request
+int idx;
+if ((idx = req.indexOf("/motor/0/on/")) != -1) {
+    // Extract everything after "/motor/0/on/"
+    String speedStr = req.substring(idx + String("/motor/0/on/").length());
+    speedStr.trim();               // remove any trailing \r, \n, spaces
+    int speed = speedStr.toInt();  // convert to integer
+    Serial.printf("Motor 0 Speed: %d\n", speed); // Debug
+    motor_on(0, speed);
+}
+
+if ((idx = req.indexOf("/motor/1/on/")) != -1) {
+    String speedStr = req.substring(idx + String("/motor/1/on/").length());
+    speedStr.trim();
+    int speed = speedStr.toInt();
+    Serial.printf("Motor 1 Speed: %d\n", speed);
+    motor_on(1, speed);
+}
+
+// Turn motors off (no speed needed)
+if (req.indexOf("/motor/0/off") != -1) motor_off(0);
+if (req.indexOf("/motor/1/off") != -1) motor_off(1);
+
 
   // Reply
   client.println("HTTP/1.1 200 OK");
