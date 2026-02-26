33a85089a2733d1f6738ad83e56037a3b826a5f2
Sethricks340 <slricks340@byui.edu>
2026-02-25 19:36:40 -0700

    Audio streaming over wifi, but it is not great quality yet

--- STC Rover/src/main.cpp
+++ STC Rover/src/main.cpp
@@ -1,191 +1,39 @@
-// // TODO: Scan for available wifi instead of hard coding
+// TODO: Scan for available wifi instead of hard coding
 
-// #include <WiFi.h>
-// #include <Arduino.h>
-// #include <ESPAsyncWebServer.h>
-// #include <ESPmDNS.h>
-
-// // WiFi credentials
-// const char* ssid = "Threat Level Midnight";
-// const char* password = "cowabunga2!!";
-// // const char* ssid = "BYUI_Visitor";
-// // const char* password = "";
-
-// // WebSocket server
-// AsyncWebServer server(81);
-// AsyncWebSocket ws("/ws");
-
-// const int IN1 = 19;
-// const int IN2 = 18;
-// const int IN3 = 25;
-// const int IN4 = 26;
-
-// const int enableA = 21;
-// const int enableB = 27;
-
-// const int freq = 5000;
-// const int channelA = 0;
-// const int channelB = 1;
-// const int resolution = 8; 
-
-// unsigned long lastPingTime = 0;
-// const unsigned long PING_INTERVAL = 500; // Send ping at 2 Hz
-// uint32_t activeClientId = 0;
-
-// void motor_on(int number, int pwm, int direction) {
-//     int inA, inB, channel;
-
-//     if (number) { // Motor 1
-//         inA = IN3;
-//         inB = IN4;
-//         channel = channelB;
-//     } else {      // Motor 0
-//         inA = IN1;
-//         inB = IN2;
-//         channel = channelA;
-//     }
-
-//     digitalWrite(inA, direction ? LOW : HIGH);
-//     digitalWrite(inB, direction ? HIGH : LOW);
-//     ledcWrite(channel, pwm);
-// }
-
-// void motor_off(int number) {
-//     int inA, inB, channel;
-
-//     if (number) {
-//         inA = IN3;
-//         inB = IN4;
-//         channel = channelB;
-//     } else {
-//         inA = IN1;
-//         inB = IN2;
-//         channel = channelA;
-//     }
-
-//     digitalWrite(inA, LOW);
-//     digitalWrite(inB, LOW);
-//     ledcWrite(channel, 0);
-// }
-
-// // WebSocket event handler
-// void onWebSocketEvent(
-//     AsyncWebSocket *server,
-//     AsyncWebSocketClient *client,
-//     AwsEventType type,
-//     void *arg,
-//     uint8_t *data,
-//     size_t len
-// ) {
-
-//     if (type == WS_EVT_CONNECT) {
-
-//         if (activeClientId != 0){
-//             Serial.printf("Rejecting client %u, already connected to client %u\n"),
-//                 client->id(), activeClientId;
-//                 client->close();
-//                 return;
-//         }
-//         activeClientId = client->id();
-//         Serial.printf("Client connected: %u\n", activeClientId);
-//     }
-
-//     else if (type == WS_EVT_DISCONNECT) {
-//         Serial.printf("Client disconnected: %u\n", client->id());
-//         if (ws.count() == 0) {
-//             Serial.println("No clients connected");
-//             activeClientId = 0;
-//             motor_off(0);
-//             motor_off(1);
-//         }
-//     }
-
-//     else if (type == WS_EVT_DATA) { // data received
-//         AwsFrameInfo *info = (AwsFrameInfo*)arg;
-//         if(info->final && info->len == 5 && info->opcode == WS_BINARY) {
-
-//             if (client->id() != activeClientId) return;
-
-//             byte opcode       = data[0];
-//             byte motor_number = data[1];
-//             byte power        = data[2];
-//             byte pwm          = data[3];
-//             byte direction    = data[4];
-
-//             Serial.print("Opcode: "); Serial.print(opcode);
-//             Serial.print(" Motor: "); Serial.print(motor_number);
-//             Serial.print(" Power: "); Serial.print(power);
-//             Serial.print(" PWM: "); Serial.print(pwm);
-//             Serial.print(" Direction: "); Serial.print(direction); Serial.print("\n");
-
-//             if(!opcode){ // Motor opcode = 0000
-//                 if (power) motor_on(motor_number, pwm, direction);
-//                 else motor_off(motor_number);
-//             }
-//         }
-//     }
-    
-//     else if (type == WS_EVT_PONG) {
-//         // Client responded to our ping - connection is alive
-//         Serial.printf("Pong received from client %u\n", client->id()); 
-//     }
-// }
-
-// void setup() {
-//     Serial.begin(921600);
-
-//     ledcSetup(channelA, freq, resolution);
-//     ledcAttachPin(enableA, channelA);
-
-//     ledcSetup(channelB, freq, resolution);
-//     ledcAttachPin(enableB, channelB);
-
-//     pinMode(IN1, OUTPUT);
-//     pinMode(IN2, OUTPUT);
-//     pinMode(IN3, OUTPUT);
-//     pinMode(IN4, OUTPUT);
-
-//     WiFi.begin(ssid, password);
-//     while (WiFi.status() != WL_CONNECTED) {
-//         delay(500);
-//         Serial.print(".");
-//     }
-
-//     // Start mDNS for friendly hostname
-//     if (MDNS.begin("stc_esp")) {
-//         Serial.println("mDNS started: stc_esp.local");
-//     }
-
-//     Serial.println();
-//     Serial.print("ESP IP: ");
-//     Serial.println(WiFi.localIP());
-
-//     ws.onEvent(onWebSocketEvent);
-//     server.addHandler(&ws);
-//     server.begin();
-// }
-
-// void loop() {
-//     // Send ping to all connected clients every 1 second
-//     if (millis() - lastPingTime > PING_INTERVAL) {
-//         lastPingTime = millis();
-//         ws.pingAll();  // Send ping to detect dead connections
-//         ws.cleanupClients();
-//     }    
-// }
-
-
-
-
-
-
-
+#include <WiFi.h>
 #include <Arduino.h>
+#include <ESPAsyncWebServer.h>
+#include <ESPmDNS.h>
 #include <driver/i2s.h>
 
+// WiFi credentials
+const char* ssid = "Threat Level Midnight";
+const char* password = "cowabunga2!!";
+// const char* ssid = "BYUI_Visitor";
+// const char* password = "";
+
+// WebSocket server
+AsyncWebServer server(81);
+AsyncWebSocket ws("/ws");
+
+const int IN1 = 19;
+const int IN2 = 18;
+const int IN3 = 25;
+const int IN4 = 26;
+
+const int enableA = 21;
+const int enableB = 27;
+
+const int freq = 5000;
+const int channelA = 0;
+const int channelB = 1;
+const int resolution = 8; 
+
+unsigned long lastPingTime = 0;
+const unsigned long PING_INTERVAL = 500; // Send ping at 2 Hz
+uint32_t activeClientId = 0;
+
 // I2S pins
-// #define I2S_WS 35
-// #define I2S_WS 25
 #define I2S_WS 14
 #define I2S_SCK 33
 #define I2S_SD 32
@@ -194,9 +42,137 @@
 #define I2S_SAMPLE_RATE 16000
 #define I2S_BUFFER_SIZE 1024
 
+void motor_on(int number, int pwm, int direction) {
+    int inA, inB, channel;
+
+    if (number) { // Motor 1
+        inA = IN3;
+        inB = IN4;
+        channel = channelB;
+    } else {      // Motor 0
+        inA = IN1;
+        inB = IN2;
+        channel = channelA;
+    }
+
+    digitalWrite(inA, direction ? LOW : HIGH);
+    digitalWrite(inB, direction ? HIGH : LOW);
+    ledcWrite(channel, pwm);
+}
+
+void motor_off(int number) {
+    int inA, inB, channel;
+
+    if (number) {
+        inA = IN3;
+        inB = IN4;
+        channel = channelB;
+    } else {
+        inA = IN1;
+        inB = IN2;
+        channel = channelA;
+    }
+
+    digitalWrite(inA, LOW);
+    digitalWrite(inB, LOW);
+    ledcWrite(channel, 0);
+}
+
+// WebSocket event handler
+void onWebSocketEvent(
+    AsyncWebSocket *server,
+    AsyncWebSocketClient *client,
+    AwsEventType type,
+    void *arg,
+    uint8_t *data,
+    size_t len
+) {
+
+    if (type == WS_EVT_CONNECT) {
+
+        if (activeClientId != 0){
+            Serial.printf("Rejecting client %u, already connected to client %u\n"),
+                client->id(), activeClientId;
+                client->close();
+                return;
+        }
+        activeClientId = client->id();
+        Serial.printf("Client connected: %u\n", activeClientId);
+    }
+
+    else if (type == WS_EVT_DISCONNECT) {
+        Serial.printf("Client disconnected: %u\n", client->id());
+        if (ws.count() == 0) {
+            Serial.println("No clients connected");
+            activeClientId = 0;
+            motor_off(0);
+            motor_off(1);
+        }
+    }
+
+    else if (type == WS_EVT_DATA) { // data received
+        AwsFrameInfo *info = (AwsFrameInfo*)arg;
+        if(info->final && info->len == 5 && info->opcode == WS_BINARY) {
+
+            if (client->id() != activeClientId) return;
+
+            byte opcode       = data[0];
+            byte motor_number = data[1];
+            byte power        = data[2];
+            byte pwm          = data[3];
+            byte direction    = data[4];
+
+            Serial.print("Opcode: "); Serial.print(opcode);
+            Serial.print(" Motor: "); Serial.print(motor_number);
+            Serial.print(" Power: "); Serial.print(power);
+            Serial.print(" PWM: "); Serial.print(pwm);
+            Serial.print(" Direction: "); Serial.print(direction); Serial.print("\n");
+
+            if(!opcode){ // Motor opcode = 0000
+                if (power) motor_on(motor_number, pwm, direction);
+                else motor_off(motor_number);
+            }
+        }
+    }
+    
+    else if (type == WS_EVT_PONG) {
+        // Client responded to our ping - connection is alive
+        Serial.printf("Pong received from client %u\n", client->id()); 
+    }
+}
+
 void setup() {
   Serial.begin(921600);
-  Serial.println("INMP441 Microphone Test - Serial Plotter");
+
+  ledcSetup(channelA, freq, resolution);
+  ledcAttachPin(enableA, channelA);
+
+  ledcSetup(channelB, freq, resolution);
+  ledcAttachPin(enableB, channelB);
+
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+  }
+
+  // Start mDNS for friendly hostname
+  if (MDNS.begin("stc_esp")) {
+      Serial.println("mDNS started: stc_esp.local");
+  }
+
+  Serial.println();
+  Serial.print("ESP IP: ");
+  Serial.println(WiFi.localIP());
+
+  ws.onEvent(onWebSocketEvent);
+  server.addHandler(&ws);
+  server.begin();
 
   // Configure I2S
   i2s_config_t i2s_config = {
@@ -227,13 +203,28 @@ void setup() {
 }
 
 void loop() {
+  // Send ping to all connected clients every 1 second
+  // if (millis() - lastPingTime > PING_INTERVAL) {
+  //     lastPingTime = millis();
+  //     ws.pingAll();  // Send ping to detect dead connections
+  //     ws.cleanupClients();
+  // }   
+  
+
+
+
   int32_t audio_samples[I2S_BUFFER_SIZE];
   size_t bytes_read;
 
   i2s_read(I2S_NUM_0, audio_samples, sizeof(audio_samples), &bytes_read, portMAX_DELAY);
 
-//   int samples = bytes_read / sizeof(int32_t);
-
-  Serial.write((uint8_t*)audio_samples, bytes_read);
-  // delay(10);
+  // Serial.write((uint8_t*)audio_samples, bytes_read);
+  // ws.binary(activeClientId, (uint8_t*)audio_samples, bytes_read);
+  if (activeClientId != 0) {
+    ws.binary(activeClientId, (uint8_t*)audio_samples, bytes_read);
+  }
 }
+
+
+
+
--- STC Rover/test/base_station_audio.py
+++ STC Rover/test/base_station_audio.py
@@ -1,67 +1,147 @@
-import serial
+# import serial
+# import numpy as np
+# import sounddevice as sd
+# import threading
+# from collections import deque
+# import time
+
+# PORT = "COM7"
+# BAUD = 921600
+# SAMPLE_RATE = 16000
+# CHUNK_SAMPLES = 1024
+# CHUNK_BYTES = CHUNK_SAMPLES * 4  # 4 bytes per int32 sample
+
+# # High-pass filter constant, blocks most of background noise
+# HPF_ALPHA = 0.9
+
+# # Fixed gain
+# GAIN = 1.0 / 50000.0
+
+# # Start the serial port
+# ser = serial.Serial(PORT, BAUD)
+# time.sleep(1.0)              # wait for ESP32 reset + I2S start
+# ser.reset_input_buffer()     # discard garbage
+
+# # Jitter buffer (stores several chunks)
+# audio_queue = deque(maxlen=10)
+
+# def serial_reader():
+#     """ Continuously read from ESP32 and push audio chunks into jitter buffer. """
+#     prev_sample = 0
+
+#     while True:
+#         raw = ser.read(CHUNK_BYTES)
+#         samples = np.frombuffer(raw, dtype=np.int32)
+
+#         # Shift 24-bit left-justified INMP441 data
+#         samples = samples >> 14
+
+#         # High-pass filter (removes DC + rumble)
+#         filtered = np.zeros_like(samples, dtype=np.float32)
+#         filtered[0] = samples[0] - prev_sample
+#         for i in range(1, len(samples)):
+#             filtered[i] = HPF_ALPHA * (filtered[i-1] + samples[i] - samples[i-1])
+#         prev_sample = samples[-1]
+
+#         # Apply fixed gain
+#         audio = filtered * GAIN
+
+#         # Clip to safe range
+#         audio = np.clip(audio, -1.0, 1.0)
+
+#         # Add to jitter buffer
+#         audio_queue.append(audio)
+
+# def audio_callback(outdata, frames, time, status):
+#     """ Fills speaker buffer with latest audio chunk. """
+#     if len(audio_queue) > 0:
+#         chunk = audio_queue.popleft()
+#         outdata[:] = chunk.reshape(-1, 1)
+#     else:
+#         # No data yet — output silence
+#         outdata[:] = np.zeros((frames, 1), dtype=np.float32)
+
+# threading.Thread(target=serial_reader, daemon=True).start()
+
+# with sd.OutputStream(
+#     samplerate=SAMPLE_RATE,
+#     channels=1,
+#     callback=audio_callback,
+#     blocksize=CHUNK_SAMPLES
+# ):
+#     print("Streaming live audio (Ctrl+C to stop)")
+#     while True:
+#         pass
+
+
+import websocket
 import numpy as np
 import sounddevice as sd
 import threading
 from collections import deque
-import time
 
-PORT = "COM7"
-BAUD = 921600
+WS_URL = "ws://stc_esp.local:81/ws"
+
 SAMPLE_RATE = 16000
 CHUNK_SAMPLES = 1024
-CHUNK_BYTES = CHUNK_SAMPLES * 4  # 4 bytes per int32 sample
-
-# High-pass filter constant, blocks most of background noise
-HPF_ALPHA = 0.995
-
-# Fixed gain
+HPF_ALPHA = 0.9
 GAIN = 1.0 / 50000.0
 
-# Start the serial port
-ser = serial.Serial(PORT, BAUD)
-time.sleep(1.0)              # wait for ESP32 reset + I2S start
-ser.reset_input_buffer()     # discard garbage
+audio_queue = deque(maxlen=20)
 
-# Jitter buffer (stores several chunks)
-audio_queue = deque(maxlen=10)
-
-def serial_reader():
-    """ Continuously read from ESP32 and push audio chunks into jitter buffer. """
+def ws_reader():
     prev_sample = 0
 
+    ws = websocket.WebSocket()
+    ws.connect(WS_URL)
+    print("WebSocket connected")
+
     while True:
-        raw = ser.read(CHUNK_BYTES)
+        # raw = ws.recv()  # receives binary frame
+        # samples = np.frombuffer(raw, dtype=np.int32)
+
+
+        raw = ws.recv()
+        print("bytes:", len(raw))
+
         samples = np.frombuffer(raw, dtype=np.int32)
+        print("samples:", len(samples),
+            "min:", samples.min() if len(samples) else None,
+            "max:", samples.max() if len(samples) else None)
 
-        # Shift 24-bit left-justified INMP441 data
-        samples = samples >> 14
+        if len(samples) == 0:
+            continue
 
-        # High-pass filter (removes DC + rumble)
+        samples = samples >> 14  # shift INMP441 data
+
         filtered = np.zeros_like(samples, dtype=np.float32)
         filtered[0] = samples[0] - prev_sample
+
         for i in range(1, len(samples)):
             filtered[i] = HPF_ALPHA * (filtered[i-1] + samples[i] - samples[i-1])
+
         prev_sample = samples[-1]
 
-        # Apply fixed gain
-        audio = filtered * GAIN
+        # audio = np.clip(filtered * GAIN, -1.0, 1.0)
+        # audio_queue.append(audio)
 
-        # Clip to safe range
-        audio = np.clip(audio, -1.0, 1.0)
+        # audio = samples.astype(np.float32) / 2**21
+        # audio_queue.append(np.clip(audio, -1.0, 1.0))
 
-        # Add to jitter buffer
+
+        audio = samples.astype(np.float32)
+        audio /= (np.max(np.abs(audio)) + 1e-9)
+        audio *= 0.8   # loud
         audio_queue.append(audio)
 
 def audio_callback(outdata, frames, time, status):
-    """ Fills speaker buffer with latest audio chunk. """
     if len(audio_queue) > 0:
         chunk = audio_queue.popleft()
-        outdata[:] = chunk.reshape(-1, 1)
+        outdata[:] = chunk[:frames].reshape(-1, 1)
     else:
-        # No data yet — output silence
         outdata[:] = np.zeros((frames, 1), dtype=np.float32)
 
-threading.Thread(target=serial_reader, daemon=True).start()
+threading.Thread(target=ws_reader, daemon=True).start()
 
 with sd.OutputStream(
     samplerate=SAMPLE_RATE,
@@ -69,6 +149,6 @@ with sd.OutputStream(
     callback=audio_callback,
     blocksize=CHUNK_SAMPLES
 ):
-    print("Streaming live audio (Ctrl+C to stop)")
+    print("Streaming WiFi audio (Ctrl+C to stop)")
     while True:
         pass
