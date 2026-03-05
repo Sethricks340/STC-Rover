4fae7b8bc197579d54cd73b6ec843c528b98e915
Sethricks340 <slricks340@byui.edu>
2026-03-04 19:25:43 -0700

    trying to get nano detection to work on pi
    this rp is annoying

--- GUI/STC_Rover_GUI.py
+++ GUI/STC_Rover_GUI.py
@@ -96,7 +96,7 @@ class SerialThread(QThread):
                     ports = serial.tools.list_ports.comports()
                     for port in ports:
                         # if "USB-SERIAL CH340" in port.description: # Search for handheld
-                        if "CH340" in port.description or "Nano" in port.description: # find nano on rp or windows
+                        if "ch340" in port.description.lower() or "nano" in port.description.lower(): # find nano on rp or windows
                             print(f"Found Handheld: {port.device}")
                             handeld = serial.Serial(port.device, 115200, timeout=1)
                             self.connection_changed.emit(True)
