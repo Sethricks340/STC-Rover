3648cf55a0e514dc88cdc9f54e8058b071a73d65
Sethricks340 <slricks340@byui.edu>
2026-03-07 08:59:02 -0700

    Update sandbox.py

--- RP_STC_Rover/sandbox.py
+++ RP_STC_Rover/sandbox.py
@@ -6,6 +6,27 @@
 import asyncio
 import websockets
 
+import RPi.GPIO as GPIO
+
+IN1 = 23
+IN2 = 24
+IN3 = 27
+IN4 = 22
+
+ENA = 18
+ENB = 19
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup([IN1,IN2,IN3,IN4], GPIO.OUT)
+GPIO.setup([ENA,ENB], GPIO.OUT)
+
+pwmA = GPIO.PWM(ENA, 1000)
+pwmB = GPIO.PWM(ENB, 1000)
+
+pwmA.start(0)
+pwmB.start(0)
+
 async def handler(websocket):
     print(f"Client connected: {websocket.remote_address}")
     try:
@@ -15,6 +36,18 @@ async def handler(websocket):
                 opcode, motor_number, power, pwm, direction = message
                 print(f"Opcode: {opcode}, Motor: {motor_number}, "
                       f"Power: {power}, PWM: {pwm}, Direction: {direction}")
+                
+
+                GPIO.output(IN1, GPIO.LOW if direction else GPIO.HIGH)
+                GPIO.output(IN2, GPIO.HIGH if direction else GPIO.LOW)
+
+                GPIO.output(IN3, GPIO.LOW if direction else GPIO.HIGH)
+                GPIO.output(IN4, GPIO.HIGH if direction else GPIO.LOW)
+
+                duty = pwm * 100 / 255
+                pwmA.ChangeDutyCycle(duty)
+                pwmB.ChangeDutyCycle(duty)
+
             else:
                 print(f"Received raw message: {message}")
     except websockets.exceptions.ConnectionClosed:
