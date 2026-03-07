609d28d3f1c1414832b2e380e2f76777f5a8b087
Sethricks340 <slricks340@byui.edu>
2026-03-07 15:45:35 -0700

    Change to tailscale ip: test

--- RP_STC_Rover/RP_STC_Rover_GUI.py
+++ RP_STC_Rover/RP_STC_Rover_GUI.py
@@ -53,6 +53,8 @@ Audio = False
 try:
     # ip_string = "ws://stc_esp.local:81/ws"  # For home Wifi
     # ip_string = "ws://10.15.56.37:81/ws"   # For byui Wifi
+    # ip_string = "ws://192.168.0.78:8081/motors"
+    # Current line in GUI:
     ip_string = "ws://192.168.0.78:8081/motors"
     ws.connect(ip_string)
     print("Controls WebSocket connected")
