35119492ad585e36ac3be71a72370108017e468f
Sethricks340 <slricks340@byui.edu>
2026-03-11 19:01:56 -0600

    Try to implement watchdog timer

--- RP_STC_Rover/motor_control.py
+++ RP_STC_Rover/motor_control.py
@@ -6,6 +6,8 @@ import RPi.GPIO as GPIO
 import time
 
 start_time = time.time()
+last_command_time = time.time()
+TIMEOUT = 0.75
 
 IN1, IN2, IN3, IN4 = 23, 24, 27, 22
 ENA, ENB = 18, 19
@@ -20,12 +22,14 @@ pwmA.start(0)
 pwmB.start(0)
 
 async def handler(websocket):
-    global start_time
+    global start_time, last_command_time
     print(f"Client connected: {websocket.remote_address}")
     try:
         async for message in websocket:
             if isinstance(message, bytes) and len(message) == 5:
                 opcode, motor_number, power, pwm, direction = message
+
+                last_command_time = time.time()
 
                 # elapsed_time = time.time() - start_time
                 # print(f'Time since last motor code: {elapsed_time} seconds')
@@ -52,7 +56,16 @@ async def handler(websocket):
     except websockets.exceptions.ConnectionClosed:
         print(f"Client disconnected: {websocket.remote_address}")
 
+async def watchdog():
+    global last_command_time
+    while True:
+        if time.time() - last_command_time > TIMEOUT:
+            pwmA.ChangeDutyCycle(0)
+            pwmB.ChangeDutyCycle(0)
+        await asyncio.sleep(0.1)
+
 async def main():
+    asyncio.create_task(watchdog())
     async with websockets.serve(handler, "0.0.0.0", 8081):
         print("WebSocket server running on ws://0.0.0.0:8081")
         await asyncio.Future()  # run forever
